eba8e8067fd7602fc94a0accf368f553001e4b4f
Soheil Moayedi Azarpour <soheil.moayedi@gmail.com>
2014-01-15 20:14:56 -0500

    Remove codes for starter project.

--- UDo/RWTableViewController.m
+++ UDo/RWTableViewController.m
@@ -31,10 +31,6 @@
 
 - (void)viewDidLoad {
   self.title = @"To Do!";
-  
-  UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPressGestureRecognized:)];
-  [self.tableView addGestureRecognizer:longPress];
-  
   [super viewDidLoad];
 }
 
@@ -98,113 +94,6 @@
   }];
   
   [inputAlertView show];
-}
-
-- (IBAction)longPressGestureRecognized:(id)sender {
-  
-  UILongPressGestureRecognizer *longPress = (UILongPressGestureRecognizer *)sender;
-  UIGestureRecognizerState state = longPress.state;
-  
-  CGPoint location = [longPress locationInView:self.tableView];
-  NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:location];
-  
-  static UIView       *snapshot = nil;        ///< A snapshot of the row user is moving.
-  static NSIndexPath  *sourceIndexPath = nil; ///< Initial index path, where gesture begins.
-  
-  switch (state) {
-    case UIGestureRecognizerStateBegan: {
-      if (indexPath) {
-        sourceIndexPath = indexPath;
-        
-        UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
-        
-        // Take a snapshot of the selected row using helper method.
-        snapshot = [self customSnapshoFromView:cell];
-        
-        // Add the snapshot as subview, centered at cell's center...
-        __block CGPoint center = cell.center;
-        snapshot.center = center;
-        snapshot.alpha = 0.0;
-        [self.tableView addSubview:snapshot];
-        [UIView animateWithDuration:0.25 animations:^{
-          
-          // Offset for gesture location.
-          center.y = location.y;
-          snapshot.center = center;
-          snapshot.transform = CGAffineTransformMakeScale(1.05, 1.05);
-          snapshot.alpha = 0.98;
-          
-          // Black out.
-          cell.backgroundColor = [UIColor blackColor];
-        } completion:nil];
-      }
-      break;
-    }
-      
-    case UIGestureRecognizerStateChanged: {
-      CGPoint center = snapshot.center;
-      center.y = location.y;
-      snapshot.center = center;
-      
-      // Is destination valid and is it different from source?
-      if (indexPath && ![indexPath isEqual:sourceIndexPath]) {
-        
-        // ... if so, move the rows.
-        [self.tableView moveRowAtIndexPath:sourceIndexPath toIndexPath:indexPath];
-        
-        // ... update data source.
-        [self moveObjectAtIndex:sourceIndexPath.row toIndex:indexPath.row];
-        
-        // ... and update source so it is in sync with UI changes.
-        sourceIndexPath = indexPath;
-      }
-      break;
-    }
-      
-    default: {
-      // Clean up.
-      UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:sourceIndexPath];
-      [UIView animateWithDuration:0.25 animations:^{
-        
-        snapshot.center = cell.center;
-        snapshot.transform = CGAffineTransformMakeScale(1.0, 1.0);
-        snapshot.alpha = 0.0;
-        
-        // Undo the black-out effect we did.
-        cell.backgroundColor = [UIColor whiteColor];
-        
-      } completion:^(BOOL finished) {
-        
-        [snapshot removeFromSuperview];
-        snapshot = nil;
-        
-      }];
-      sourceIndexPath = nil;
-      break;
-    }
-  }
-}
-
-#pragma mark - Helper methods
-
-/** @brief Returns a customized snapshot of a given view. */
-- (UIView *)customSnapshoFromView:(UIView *)inputView {
-  
-  UIView *snapshot = [inputView snapshotViewAfterScreenUpdates:YES];
-  snapshot.layer.masksToBounds = NO;
-  snapshot.layer.cornerRadius = 0.0;
-  snapshot.layer.shadowOffset = CGSizeMake(-5.0, 0.0);
-  snapshot.layer.shadowRadius = 5.0;
-  snapshot.layer.shadowOpacity = 0.4;
-  
-  return snapshot;
-}
-
-/** @brief Moves object in data source array from one index to another index. */
-- (void)moveObjectAtIndex:(NSUInteger)fromIndex toIndex:(NSUInteger)toIndex {
-  id objectToMove = self.objects[fromIndex];
-  [self.objects insertObject:objectToMove atIndex:toIndex];
-  [self.objects removeObjectAtIndex:fromIndex];
 }
 
 @end
