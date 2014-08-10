a7650b6783a4849af42dee7e9014d48972e83a93
Soheil Moayedi Azarpour <a523575@fmr.com>
2014-08-10 15:47:42 -0400

    Fade out and hide the table view cell, instead of changing its background color.

---
+++ UDo/RWBasicTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  RWBasicTableViewCell.h
+//  UDo
+//
+//  Created by Soheil M. Azarpour on 8/9/14.
+//  Copyright (c) 2014 Soheil Azarpour. All rights reserved.
+//
+
+@import UIKit;
+
+@interface RWBasicTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
+@end
